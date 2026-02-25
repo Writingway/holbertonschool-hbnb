03bd9162420bc838cee093b31e5a7d3be1735609
Yasi-Philippe <shyasi95@gmail.com>
2026-02-25 16:11:27 +0100

    feat: Amenities fully functional to retrieve with the API

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 
 
 def create_app():
@@ -11,5 +12,6 @@ def create_app():
     # Additional namespaces for places, reviews, and amenities will be added later
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
     return app
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -38,3 +38,25 @@ class HBnBFacade:
         }
         self.user_repo.update(user_id, new_user)
         return new_user
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.get_amenity(amenity_id)
+        if not amenity:
+            return None
+        new_amenity = {
+            "id": amenity_id,
+            "name": amenity_data.get("name")
+            }
+        self.amenity_repo.update(amenity_id, new_amenity)
+        return new_amenity
